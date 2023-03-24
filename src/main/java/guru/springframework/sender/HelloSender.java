4bb630bc72f192772d985bd7a6215bef0135dfc1
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-03-24 18:43:12 +0100

    added some extra functions

--- src/main/java/guru/springframework/sender/HelloSender.java
+++ src/main/java/guru/springframework/sender/HelloSender.java
@@ -1,32 +1,76 @@
 package guru.springframework.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.config.JmsConfig;
 import guru.springframework.model.HelloWorldMessage;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+/**
+ * Created by jt on 2019-07-17.
+ */
 @RequiredArgsConstructor
 @Component
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
+
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
-
-        System.out.println("I'm sending a message");
 
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
                 .id(UUID.randomUUID())
-                .message("Hello World")
+                .message("Hello World!")
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-        System.out.println("Message sent!");
     }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendandReceiveMessage() throws JmsException, JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receviedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JmsException, JMSException {
+                Message helloMessage = null;
+
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+
+                    System.out.println("Sending Hello");
+
+                    return helloMessage;
+
+                } catch (JsonProcessingException e) {
+                    throw new JmsException("boom") {
+                    };
+                }
+            }
+        });
+
+        assert receviedMsg != null;
+        System.out.println(receviedMsg.getBody(String.class));
+
+    }
+
 }
